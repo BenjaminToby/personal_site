5c855e092f53fac9370c48fef7e252711aade0ec
BenjaminToby <52448020+BenjaminToby@users.noreply.github.com>
2021-06-04 21:11:31 +0100

    Scroll animations refined

--- scripts/v2.js
+++ scripts/v2.js
@@ -4,16 +4,20 @@
 
 const translate = document.querySelectorAll(".translate");
 var watermark = document.querySelector(".hero-watermark");
-var skewedBg = document.querySelector(".skewed-bg");
+//var skewedBg = document.querySelector(".skewed-bg");
 
 
 
 window.addEventListener("scroll", () => {
    
     var scroll = window.pageYOffset;
+    
     translate.forEach(element => {
         let speed = element.dataset.speed;
         element.style.transform = "translateY(" + (scroll * speed) + "px)";
     });
+    
+//    skewedBg.style.transform = "translateY(" + (scroll * skewedBg.dataset.speed) + "px) skewY(-3deg)";
+    watermark.style.transform = "translateY(" + (scroll * watermark.dataset.speed) + "px) translateX(-50%)";
     
 });
