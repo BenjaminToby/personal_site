7e2ba3dcefde2d1ddf7358a8f6a3887213985cbd
Tben <benoti.san@gmail.com>
2023-08-21 09:28:30 +0100

    updates

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,7 +1,17 @@
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request, context?: {}) {
-    console.log(request.headers);
+// export async function GET(request: Request, context?: {}) {
+//     console.log(request.headers);
 
-    return NextResponse.json({ name: "Benjamin Toby" });
+//     return NextResponse.json({ name: "Benjamin Toby" });
+// }
+export async function GET(request: Request) {
+    return new Response(JSON.stringify({ name: "Benjamin Toby" }), {
+        status: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        },
+    });
 }
