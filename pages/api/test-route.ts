46272927860a70509573520c437f0c31747d95b9
Tben <benoti.san@gmail.com>
2023-08-21 09:15:10 +0100

    updates

--- pages/api/test-route.ts
+++ pages/api/test-route.ts
@@ -4,7 +4,7 @@ import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
  * @type {NextApiHandler}
  */
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    return res.status(200).json({
+    res.status(200).json({
         title: "Hello There",
         message: "General Kenobi",
     });
