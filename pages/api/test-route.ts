51a02ac5f5718c93cc7a2536112cdedfff7e39eb
Tben <benoti.san@gmail.com>
2023-08-21 09:32:27 +0100

    updates

--- pages/api/test-route.ts
+++ pages/api/test-route.ts
@@ -1,12 +1,33 @@
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+import Cors from "cors";
+
+const cors = Cors({
+    methods: ["POST", "GET", "HEAD"],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: Function) {
+    return new Promise((resolve, reject) => {
+        fn(req, res, (result: any) => {
+            if (result instanceof Error) {
+                return reject(result);
+            }
+
+            return resolve(result);
+        });
+    });
+}
 
 /**
  * @type {NextApiHandler}
  */
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "*");
-    res.setHeader("Access-Control-Allow-Headers", "*");
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    // res.setHeader("Access-Control-Allow-Origin", "*");
+    // res.setHeader("Access-Control-Allow-Methods", "*");
+    // res.setHeader("Access-Control-Allow-Headers", "*");
+
+    await runMiddleware(req, res, cors);
 
     res.status(200).json({
         title: "Hello There",
