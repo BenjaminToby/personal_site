44729c68449cbd1c509dea942c67fe64bd4b9b2c
Tben <benoti.san@gmail.com>
2023-08-21 09:18:53 +0100

    updates

--- pages/api/test-route.ts
+++ pages/api/test-route.ts
@@ -4,6 +4,10 @@ import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
  * @type {NextApiHandler}
  */
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+
     res.status(200).json({
         title: "Hello There",
         message: "General Kenobi",
