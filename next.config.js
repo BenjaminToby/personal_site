2b094160cd25bfc45ac44bcab345e82003129f74
Tben <benoti.san@gmail.com>
2023-08-21 09:42:08 +0100

    updates

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ module.exports = {
     async headers() {
         return [
             {
-                source: "/api/test-route",
+                source: "/api/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" },
