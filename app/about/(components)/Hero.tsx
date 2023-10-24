b9364a702ce30d301e83ff349d8a02ba89c31a4e
Benjamin Toby <benoti.san@gmail.com>
2023-10-24 18:59:00 +0100

    Updates

--- app/about/(components)/Hero.tsx
+++ app/about/(components)/Hero.tsx
@@ -10,13 +10,17 @@ export default function Hero() {
                 // id="hero-text-section"
             >
                 <span className="text-primary-light text-lg">About Me</span>
-                <h1
-                    className="text-5xl leading-snug"
-                    id="hero-text"
-                >
-                    Ben of All Trades, Master of All
+                <h1 className="text-5xl leading-snug" id="hero-text">
+                    I live on the bleeding edge of technology
                 </h1>
-                <span className="hero-sub-text">Quick learner, adaptable, problem solver, curious. I strive to know the system, rather than the status quo. My credo is: no problem too great, no knowledge too vast, no logic too complex. I thrive in difficult situations and complex hurdles: problem solving is now second nature to me: if you can think it, it can be done.</span>
+                <span className="hero-sub-text">
+                    Quick learner, adaptable, problem solver, curious. I strive
+                    to know the system, rather than the status quo. My credo is:
+                    no problem too great, no knowledge too vast, no logic too
+                    complex. I thrive in difficult situations and complex
+                    hurdles: problem solving is now second nature to me: if you
+                    can think it, it can be done.
+                </span>
             </div>
         </div>
     );
