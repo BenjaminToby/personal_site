a2ee271397945d512e58e012ff74a6b4856fe282
Benjamin Toby <benoti.san@gmail.com>
2023-11-23 17:52:26 +0100

    Update Contact section on homepage

--- app/(components)/ContactMeSection.tsx
+++ app/(components)/ContactMeSection.tsx
@@ -9,7 +9,7 @@ export default function ContactMeSection() {
             <div className="flex flex-col items-start gap-6 w-full xl:w-[50%]">
                 <div className="h-[150px]"></div>
 
-                <h2 className="m-0">Why So Serious?</h2>
+                <h2 className="m-0">Contact Me.</h2>
 
                 <div className="flex flex-col-reverse xl:flex-col items-start gap-4">
                     <span className="text-[24px]">Let's talk.</span>
